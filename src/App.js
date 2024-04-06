a61cd7fdb0bbf1f37cf5f19473a9eca83bdc95a9
Vakle3360 <vakleondiscord@gmail.com>
2024-04-07 00:11:36 +0200

    home-page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+"use client";
+
 import React, {useState, useEffect} from 'react'
-import UserList from './UserList';
+//import UserList from './UserList';
 import './index.css';
-import UserForm from './UserForm';
+//import UserForm from './UserForm';
 
 function App() {
-  
+//#region DB  
   const [users, setUsers] = useState([{"id": 1,"fornavn": "ole", "efternavn": "henriksen", "alder": 18, "email": "ole@henriksen.dk", "adgangskode": "facts" }]);
 
   useEffect (() => {
-    fetchUsers()
+    fetchUsers();
   }, [])
 
   const fetchUsers = async () => {
@@ -16,13 +18,23 @@ function App() {
     const data = await repsonse.json();
     setUsers(data.users);    
   }
+//#endregion
+
+
+  const onLocation = () => {
+    let a_ = false;
+    if(document.getElementById("dest").value === ""){
+      a_ = true;
+    }
+    document.getElementById("blaff-btn").disabled = a_;
+  }
 
   return (
     <>
       <h1 style={{marginTop: '55px'}}>Vælg destination</h1>
       <div id="kort"></div>
       <h1 style={{marginTop: '26px'}}>Eller</h1>
-      <input type='text' placeholder='Indtast destination' style={{marginTop: '22px'}}></input>
+      <input id="dest" type='text' placeholder='Indtast destination' style={{marginTop: '22px'}} onInput={() => {onLocation()}}></input>
       <button id="blaff-btn" disabled>Blaff</button>
     </>
   )
