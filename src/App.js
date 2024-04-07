b806c60a10edd28246a8aea91841cae09b8fc140
Vakle3360 <vakleondiscord@gmail.com>
2024-04-07 15:06:45 +0200

    map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-"use client";
-
 import React, {useState, useEffect} from 'react'
 //import UserList from './UserList';
 import './index.css';
 //import UserForm from './UserForm';
+import kort from './img/kort.png'; 
 
 function App() {
 //#region DB  
@@ -19,20 +18,38 @@ function App() {
     setUsers(data.users);    
   }
 //#endregion
+//#region kort
+  const x = document.getElementById("dest");
 
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+     x.value = ("Geolocation is not supported by this browser.");
+    }
+  }
+
+  const showPosition = (position) => {
+    x.value = position.coords.latitude +
+    ", " + position.coords.longitude;
+    onLocation();
+  }
 
   const onLocation = () => {
     let a_ = false;
-    if(document.getElementById("dest").value === ""){
+    if(x.value === ""){
       a_ = true;
     }
     document.getElementById("blaff-btn").disabled = a_;
   }
+//#endregion
 
   return (
     <>
       <h1 style={{marginTop: '55px'}}>Vælg destination</h1>
-      <div id="kort"></div>
+      <div id="kort" onClick={() => {getLocation()}}>
+        <img src={kort} style={{width: "180px", marginLeft: "51px", marginTop: "22px"}} alt=''/>
+      </div>
       <h1 style={{marginTop: '26px'}}>Eller</h1>
       <input id="dest" type='text' placeholder='Indtast destination' style={{marginTop: '22px'}} onInput={() => {onLocation()}}></input>
       <button id="blaff-btn" disabled>Blaff</button>
